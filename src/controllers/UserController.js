8227fdf087bd851483844fe7d577293463b70cec
Bruno Brilhante <brunobrilhante53@gmail.com>
2024-07-02 17:20:24 -0300

    handling errors

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,26 +1,42 @@
 const User = require('../models/User')
 
 async function getUsers(req, res) {
-    const users = await User.find()
-    return res.status(200).json(users)
+    try {
+        const users = await User.find()
+        return res.status(200).json(users)        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
 
 async function createUser(req, res) {
-    const user = req.body
-    const newUser = await User.create(user)
-    return res.status(201).json(newUser)
+    try {
+        const user = req.body
+        const newUser = await User.create(user)
+        return res.status(201).json(newUser)        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
 
 async function deleteUser(req, res) {
-    const id = req.params.id
-    await User.findByIdAndDelete({ _id: id })
-    return res.status(200).json({ response: 'User deleted' })
+    try {
+        const id = req.params.id
+        await User.findByIdAndDelete({ _id: id })
+        return res.status(200).json({ response: 'User deleted' })
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
 
 async function updateUser(req, res) {
-    const id = req.params.id
-    const updatedUser = await User.findOneAndUpdate({ _id: id }, { name: req.body.name, age: req.body.age })
-    return res.status(400).json(updatedUser)
+    try {
+        const id = req.params.id
+        const updatedUser = await User.findOneAndUpdate({ _id: id }, { name: req.body.name, age: req.body.age })
+        return res.status(200).json(updatedUser)        
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
 }
 
 module.exports = { createUser, getUsers, deleteUser, updateUser }
